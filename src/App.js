55fff273eca53affd9c99d4146e9a649b617980e
MailsonSilva <mailsonfcsilva@gmail.com>
2020-06-17 14:42:35 -0300

    Primeiro desafio GoStack ReactJS

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+import api from './services/api';
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  },[]);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories',{
+      title: "Desafio React Native",
+      url: "https://github.com/MailsonSilva",
+      techs: [
+        "NodeJS",
+        "ReactJS",
+        "React Native"
+      ]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  };
+
+  async function handleRemoveRepository(id) {    
+    await api.delete(`repositories/${id}`);
+    const filteredRepositories = repositories.filter(repository => repository.id !== id );
+    setRepositories(filteredRepositories);    
+  };
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}            
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>          
+        )}        
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar</button>      
+    </div>
+  );
+}
+
+export default App;
